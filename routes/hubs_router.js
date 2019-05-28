afb5698bd1404e0147bd539e9d8fe0dec3ff03a3
Andy Bettisworth <andy@accreu.com>
2019-05-28 12:26:55 -0500

    Remove comments and add whitespace

--- routes/hubs_router.js
+++ routes/hubs_router.js
@@ -19,12 +19,12 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    console.log("HUBS")
     const hubs = await Hub.find(req.query)
+
     res.status(200).json(hubs)
   } catch (error) {
-    // log error to database
-    console.log(error)
+    console.error(error)
+
     res.status(500).json({
       message: 'Error retrieving the hubs',
     })
@@ -41,8 +41,8 @@ router.get('/:id', async (req, res) => {
       res.status(404).json({ message: 'Hub not found' })
     }
   } catch (error) {
-    // log error to database
-    console.log(error)
+    console.error(error)
+
     res.status(500).json({
       message: 'Error retrieving the hub',
     })
@@ -52,10 +52,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const hub = await Hub.add(req.body)
+
     res.status(201).json(hub)
   } catch (error) {
-    // log error to database
-    console.log(error)
+    console.error(error)
+
     res.status(500).json({
       message: 'Error adding the hub',
     })
@@ -65,14 +66,15 @@ router.post('/', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const count = await Hub.remove(req.params.id)
+
     if (count > 0) {
       res.status(200).json({ message: 'The hub has been nuked' })
     } else {
       res.status(404).json({ message: 'The hub could not be found' })
     }
   } catch (error) {
-    // log error to database
-    console.log(error)
+    console.error(error)
+
     res.status(500).json({
       message: 'Error removing the hub',
     })
@@ -82,14 +84,15 @@ router.delete('/:id', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const hub = await Hub.update(req.params.id, req.body)
+
     if (hub) {
       res.status(200).json(hub)
     } else {
       res.status(404).json({ message: 'The hub could not be found' })
     }
   } catch (error) {
-    // log error to database
-    console.log(error)
+    console.error(error)
+
     res.status(500).json({
       message: 'Error updating the hub',
     })
